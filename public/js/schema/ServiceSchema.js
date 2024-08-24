d64b7937f016acc976e9ea6da4f16c91c772fabf
Zouhir Bensmaia <freelancebenz@gmail.com>
2024-08-24 10:24:44 -0400

    Added backlink from drywallkingston to pavagegatineau

--- public/js/schema/ServiceSchema.js
+++ public/js/schema/ServiceSchema.js
@@ -4,7 +4,7 @@ const serviceSchema = {
   "serviceType": "Drywall Services",
   "provider": {
     "@type": "LocalBusiness",
-    "name": "Ace Drywall Services",
+    "name": "Drywall Kingston Earnanswers",
     "description": "We provide top-notch drywall installation and repair services for residential and commercial properties in the Kingston area.",
     "address": {
       "@type": "PostalAddress",
@@ -15,7 +15,7 @@ const serviceSchema = {
       "addressCountry": "Canada"
     },
     "telephone": "+1 (343) 477 1480",
-    "email": "info@acedrywall.com",
+    "email": "drywallkingston@outlook.com",
 
     "openingHoursSpecification": [
       {
@@ -28,7 +28,7 @@ const serviceSchema = {
           "Friday"
         ],
         "opens": "09:00",
-        "closes": "17:00"
+        "closes": "18:00"
       }
     ],
 
@@ -49,11 +49,11 @@ const serviceSchema = {
             "serviceType": "Drywall Installation",
             "name": "Drywall Installation",
             "description": "Expert Drywall Installation Services for Residential and Commercial Projects",
-            "image": "https://example.com/drywall-installation.jpg",
-            "url": "https://example.com/service/drywall-installation",
+            "image": "https://drywallkingston.com/img/drywall-installation.avif",
+            "url": "https://drywallkingston.com/service/drywall-installation",
             "provider": {
               "@type": "LocalBusiness",
-              "name": "Ace Drywall Services"
+              "name": "Drywall Kingston Earnanswers"
             }
           },
           "priceCurrency": "CAD",
@@ -66,11 +66,11 @@ const serviceSchema = {
             "serviceType": "Drywall Repair and Patching",
             "name": "Drywall Repair and Patching",
             "description": "Reliable Drywall Repair and Patching Services for Homes and Businesses",
-            "image": "https://example.com/drywall-repair-and-patching.jpg",
-            "url": "https://example.com/service/drywall-repair-and-patching",
+            "image": "https://drywallkingston.com/img/drywall-repair.avif",
+            "url": "https://drywallkingston.com/service/drywall-repair-and-patching",
             "provider": {
               "@type": "LocalBusiness",
-              "name": "Ace Drywall Services"
+              "name": "Drywall Kingston Earnanswers"
             }
           },
           "priceCurrency": "CAD",
@@ -83,11 +83,11 @@ const serviceSchema = {
             "serviceType": "Drywall Finishing and Texturing",
             "name": "Drywall Finishing and Texturing",
             "description": "Professional Drywall Finishing and Texturing for a Perfect Finish",
-            "image": "https://example.com/drywall-finishing-and-texturing.jpg",
-            "url": "https://example.com/service/drywall-finishing-and-texturing",
+            "image": "https://drywallkingston.com/img/drywall-texturing.avif",
+            "url": "https://drywallkingston.com/service/drywall-finishing-and-texturing",
             "provider": {
               "@type": "LocalBusiness",
-              "name": "Ace Drywall Services"
+              "name": "Drywall Kingston Earnanswers"
             }
           },
           "priceCurrency": "CAD",
@@ -145,150 +145,6 @@ const serviceSchema = {
 }
 
 
-
-
-// const localbusinessSchema =
-// {
-//   "@context": "https://schema.org",
-//   "@type": "LocalBusiness",
-//   "name": "Ace Drywall Services",
-//   "description": "We provide top-notch drywall installation and repair services for residential and commercial properties in the Kingston area.",
-//   "address": {
-//     "@type": "PostalAddress",
-//     "streetAddress": "1786 Bath Rd #18",
-//     "addressLocality": "Kingston",
-//     "addressRegion": "ON",
-//     "postalCode": "K7M 4Y2",
-//     "addressCountry": "Canada"
-//   },
-//   "telephone": "+1 (343) 477 1480",
-//   "email": "info@acedrywall.com",
-
-//   "openingHoursSpecification": [
-//     {
-//       "@type": "OpeningHoursSpecification",
-//       "dayOfWeek": [
-//         "Monday",
-//         "Tuesday",
-//         "Wednesday",
-//         "Thursday",
-//         "Friday"
-//       ],
-//       "opens": "09:00",
-//       "closes": "17:00"
-//     }
-//   ],
-
-//   "geo": {
-//     "@type": "GeoCoordinates",
-//     "latitude": 43.6532,
-//     "longitude": -79.3832
-//   },
-
-//   "hasOfferCatalog": {
-//     "@type": "OfferCatalog",
-//     "name": "Service Catalog",
-//     "itemListElement": [
-//       {
-//         "@type": "Offer",
-//         "itemOffered": {
-//           "@type": "Service",
-//           "serviceType": "Drywall Installation",
-//           "name": "Drywall Installation",
-//           "description": "Expert Drywall Installation Services for Residential and Commercial Projects",
-//           "image": "https://example.com/drywall-installation.jpg",
-//           "url": "https://example.com/service/drywall-installation",
-//           "provider": {
-//             "@type": "LocalBusiness",
-//             "name": "Ace Drywall Services"
-//           }
-//         },
-//         "priceCurrency": "CAD",
-//         // "price": "50.00"
-//       },
-//       {
-//         "@type": "Offer",
-//         "itemOffered": {
-//           "@type": "Service",
-//           "serviceType": "Drywall Repair and Patching",
-//           "name": "Drywall Repair and Patching",
-//           "description": "Reliable Drywall Repair and Patching Services for Homes and Businesses",
-//           "image": "https://example.com/drywall-repair-and-patching.jpg",
-//           "url": "https://example.com/service/drywall-repair-and-patching",
-//           "provider": {
-//             "@type": "LocalBusiness",
-//             "name": "Ace Drywall Services"
-//           }
-//         },
-//         "priceCurrency": "CAD",
-//         // "price": "30.00"
-//       },
-//       {
-//         "@type": "Offer",
-//         "itemOffered": {
-//           "@type": "Service",
-//           "serviceType": "Drywall Finishing and Texturing",
-//           "name": "Drywall Finishing and Texturing",
-//           "description": "Professional Drywall Finishing and Texturing for a Perfect Finish",
-//           "image": "https://example.com/drywall-finishing-and-texturing.jpg",
-//           "url": "https://example.com/service/drywall-finishing-and-texturing",
-//           "provider": {
-//             "@type": "LocalBusiness",
-//             "name": "Ace Drywall Services"
-//           }
-//         },
-//         "priceCurrency": "CAD",
-//         // "price": "40.00"
-//       }
-//     ]
-//   },
-//   "review": [
-//     {
-//       "@type": "Review",
-//       "author": {
-//         "@type": "Person",
-//         "name": "Doug Leo"
-//       },
-//       "reviewRating": {
-//         "@type": "Rating",
-//         "ratingValue": "5",
-//         "bestRating": "5"
-//       },
-//       "reviewBody": "Exceptional drywall service! Efficient, professional, and great results. Highly recommend them for any job."
-//     },
-//     {
-//       "@type": "Review",
-//       "author": {
-//         "@type": "Person",
-//         "name": "Alan Schmidts"
-//       },
-//       "reviewRating": {
-//         "@type": "Rating",
-//         "ratingValue": "5",
-//         "bestRating": "5"
-//       },
-//       "reviewBody": "Great experience with this Kingston drywall company. Prompt, skilled, and the finish was flawless. Very impressed."
-//     },
-//     {
-//       "@type": "Review",
-//       "author": {
-//         "@type": "Person",
-//         "name": "Hal 'Haly' Lowie"
-//       },
-//       "reviewRating": {
-//         "@type": "Rating",
-//         "ratingValue": "4.5",
-//         "bestRating": "5"
-//       },
-//       "reviewBody": "Top-notch drywall repair. Friendly team, quick service, and perfect results. Will definitely hire again."
-//     }
-//   ],
-//   "aggregateRating": {
-//     "@type": "AggregateRating",
-//     "ratingValue": "4.8",
-//     "reviewCount": "3"
-//   }
-// }
 
 
 const script2 = document.createElement('script');
